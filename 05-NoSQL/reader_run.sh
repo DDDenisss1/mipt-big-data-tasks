96a2c4cf2ffe6e9ab87063321c1161fb1d9f663b
DDDenisss1 <d.bessmertnyi@outlook.com>
2026-09-09 23:29:31 +0300

    Create reader_run.sh

---
+++ 05-NoSQL/reader_run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# если нужно вывести все матчи
+python3 reader.py --start 100 --end 500
+
+#!/usr/bin/env bash
+
+# если нужно вывести один матч
+python3 reader.py \
+  --match 2U4GBNA0Ymk---Hambc68xkUoMaf9WzIZGGxC5vpt50iN2hdzgvov5BiQdj7lj7S \
+  --start 100 \
+  --end 500
