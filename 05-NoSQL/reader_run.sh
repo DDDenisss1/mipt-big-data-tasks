0857da73a26044cec27bdbe499a314dac5ca5d6d
DDDenisss1 <d.bessmertnyi@outlook.com>
2026-09-09 23:30:03 +0300

    Update reader_run.sh

--- 05-NoSQL/reader_run.sh
+++ 05-NoSQL/reader_run.sh
@@ -3,8 +3,6 @@
 # если нужно вывести все матчи
 python3 reader.py --start 100 --end 500
 
-#!/usr/bin/env bash
-
 # если нужно вывести один матч
 python3 reader.py \
   --match 2U4GBNA0Ymk---Hambc68xkUoMaf9WzIZGGxC5vpt50iN2hdzgvov5BiQdj7lj7S \
